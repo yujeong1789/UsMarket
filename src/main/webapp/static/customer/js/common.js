f8605d7754b7c4c8cba84084b8fba00feef69312
yujeong <www_17@naver.com>
2023-02-01 20:09:09 +0900

    Feat: generateRandomString, getOrderNo 함수 추가

--- src/main/webapp/static/customer/js/common.js
+++ src/main/webapp/static/customer/js/common.js
@@ -8,4 +8,36 @@ function isEmpty(value) {
 	} else {
 		return false;
 	}
-};
+}
+
+// 10보다 작은 값에 0을 붙임 (1월 -> 01월)
+function leftPad(value) {
+	if (value>=10){
+		return value
+	}
+	return '0'+value;
+}
+
+// 현재 시간을 리턴
+function getCurrentDate() {
+	var d = new Date();
+	return (d.getFullYear().toString().slice(2)) + leftPad(d.getMonth()+1) + leftPad(d.getDate()) + leftPad(d.getHours()) + leftPad(d.getMinutes()) + d.getSeconds() + d.getMilliseconds();
+}
+
+// 특정 길이의 랜덤한 문자열을 반환하는 함수
+const generateRandomString = (num) => {
+	const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	let result = '';
+	const charactersLength = characters.length;
+	
+	for (let i = 0; i < num; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+
+	return result;
+}
+
+function getOrderNo(num){
+	var d = new Date();
+	return d.getFullYear() + leftPad(d.getMonth()+1) + leftPad(d.getDate()) + generateRandomString(num);
+}
