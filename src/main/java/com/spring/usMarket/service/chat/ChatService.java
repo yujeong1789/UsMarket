2cad47c7aa0aab7bc33d54308e95ce20da3e9921
yujeong <www_17@naver.com>
2023-02-11 22:12:23 +0900

    Refactor: ChatService getNickName 메서드 삭제

--- src/main/java/com/spring/usMarket/service/chat/ChatService.java
+++ src/main/java/com/spring/usMarket/service/chat/ChatService.java
@@ -8,7 +8,6 @@ import com.spring.usMarket.domain.chat.ChatRoomDto;
 
 public interface ChatService {
 	ChatRoomDto getChatRoom(Integer chat_member_1, Integer chat_member_2) throws Exception;
-	String getNickName(Integer member_no) throws Exception;
 	List<Map<String, Object>> getChatList(Integer member_no) throws Exception;
-	List<ChatDto> getChatInfo (String room_no) throws Exception;
+	List<ChatDto> getChatInfo (String room_no, String is_read, Integer chat_to) throws Exception;
 }
