48d25da653fcbf2148344886ab16f375af61f01b
yujeong <www_17@naver.com>
2023-02-15 22:20:00 +0900

    Feat: EchoHandler 수정

--- src/main/java/com/spring/usMarket/handler/EchoHandler.java
+++ src/main/java/com/spring/usMarket/handler/EchoHandler.java
@@ -3,6 +3,8 @@ package com.spring.usMarket.handler;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.CloseStatus;
@@ -12,35 +14,40 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 // 핸들러 구현
 public class EchoHandler extends TextWebSocketHandler{
-	private static final Logger logger = LoggerFactory.getLogger(EchoHandler.class);
 	
-	// private static List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
+	private static final Logger logger = LoggerFactory.getLogger(EchoHandler.class);
 	private static Map<String, WebSocketSession> sessionList = new ConcurrentHashMap<>();
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		// 연결이 수립된 직후 실행
-		logger.info("connected! session.getId() = {}", session.getId());
-		// sessionList.add(session);
-		sessionList.put(session.getId(), session);
+		
+		String senderNo = String.valueOf(session.getAttributes().get("userNo"));
+		logger.info("connected! senderNo = {}", senderNo);
+		
+		sessionList.put(senderNo, session);
+		
+		logger.info("sessionList.size() = {}", sessionList.size());
 	}
 		
 		
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		// 클라이언트가 메세지를 보냈을 때 실행
 		
-		logger.info("handleTextMessage {}: {}", session.getId(), message);
+		String senderNo = String.valueOf(session.getAttributes().get("userNo"));
+		logger.info("handleTextMessage senderNo = {}, payload = {}", senderNo, message.getPayload().toString());
 		
-		/*for(WebSocketSession sess : sessionList) { // 연결되어있는 동안 실행
-			//페이로드 - 헤더, 메타데이터 등 부수적인 정보 제외한 보내고자 하는 데이터
-			sess.sendMessage(new TextMessage(session.getId()+": "+message.getPayload()));
-		}*/
+		String msg = message.getPayload();
 		
-		String msg = message.getPayload(); // js에서 보낸 세션 아이디 값
-		for(WebSocketSession sess : sessionList.values()) {
-			if(sess != null) {
-				sess.sendMessage(new TextMessage(session.getId()+": "+message.getPayload()));				
+		if(!msg.isEmpty()) {
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject = (JSONObject)jsonParser.parse(msg);
+			
+			String receiverNo = String.valueOf(jsonObject.get("chat_to"));
+			String chatContent = String.valueOf(jsonObject.get("chat_content"));
+			
+			if(sessionList.get(receiverNo) != null) {
+				WebSocketSession receiver = sessionList.get(receiverNo);
+				receiver.sendMessage(new TextMessage(chatContent));
 			}
 		}
 		
@@ -49,17 +56,11 @@ public class EchoHandler extends TextWebSocketHandler{
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		// 연결이 끊어진 후 remove로 해당 세션 제거
-		logger.info("disConnected! session.getId() = {}", session.getId());
-		sessionList.remove(session.getId());
+		
+		String senderNo = String.valueOf(session.getAttributes().get("userNo"));
+		logger.info("disConnected! senderNo = {}", senderNo);
+		
+		sessionList.remove(senderNo);
 	}
-	
-	
-	/*
-	// 세션에 저장된 사용자 이름 얻어오기
-	public String searchId(WebSocketSession session) throws Exception{
-		Map<String, Object>map=session.getAttributes();
-		return (String)map.get("userId");
-	}*/
 
 }
