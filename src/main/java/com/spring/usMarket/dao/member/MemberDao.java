8e3a7051a828f5b98a754946cfdf6fd06c7e3d95
Kim Donghyuon <ehd1530@naver.com>
2023-02-16 18:43:09 +0900

    member_img테이블 관련 코드 삭제, 회원 정보 검색 코드 추가

--- src/main/java/com/spring/usMarket/dao/member/MemberDao.java
+++ src/main/java/com/spring/usMarket/dao/member/MemberDao.java
@@ -3,14 +3,13 @@ package com.spring.usMarket.dao.member;
 import java.util.Map;
 
 import com.spring.usMarket.domain.member.MemberDto;
-import com.spring.usMarket.domain.member.MemberFileDto;
 
 public interface MemberDao {
 	int insertMember(MemberDto member);
-	int insertMemberFile(MemberFileDto memberFileDto) throws Exception;	
 	int overlappedID(String member_id);
 	int overlappedNick(String member_nick);
 	int overlappedEmail(String member_email);
 	void overlapped(String joinMember);
 	Map<String, Object> idLogin(String member_id) throws Exception;
+	Map<Integer, Object> memberSearche(Integer member_no) throws Exception;
 }
