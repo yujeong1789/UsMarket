87f6fce82f7b4cf68cb059ed6ef00ce5df446594
yujeong <www_17@naver.com>
2023-03-10 19:51:52 +0900

    Refactor: ReportService addReport 매개변수 수정

--- src/main/java/com/spring/usMarket/service/report/ReportService.java
+++ src/main/java/com/spring/usMarket/service/report/ReportService.java
@@ -3,7 +3,9 @@ package com.spring.usMarket.service.report;
 import java.util.List;
 import java.util.Map;
 
+import com.spring.usMarket.domain.report.ReportInsertDto;
+
 public interface ReportService {
-	int addReport(Map<String, Object> map) throws Exception;
+	int addReport(ReportInsertDto dto) throws Exception;
 	List<Map<String, Object>> getReportCategory2(Integer report_category2_no) throws Exception;
 }
