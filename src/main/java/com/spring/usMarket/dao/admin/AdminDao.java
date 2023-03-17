3a1422564196c214481510a9cd76ef0fe06c1630
yujeong <www_17@naver.com>
2023-03-17 23:37:06 +0900

    Feat: AdminDao searchMemberStatsPreview 추가

--- src/main/java/com/spring/usMarket/dao/admin/AdminDao.java
+++ src/main/java/com/spring/usMarket/dao/admin/AdminDao.java
@@ -1,7 +1,9 @@
 package com.spring.usMarket.dao.admin;
 
+import java.util.List;
 import java.util.Map;
 
 public interface AdminDao {
 	Map<String, Object> searchAdmin(String admin_id, String admin_password) throws Exception;
+	List<Map<String, Object>> searchMemberStatsPreview(String startDate, String endDate) throws Exception;
 }
