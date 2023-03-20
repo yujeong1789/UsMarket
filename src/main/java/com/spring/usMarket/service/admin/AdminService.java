4672cbab182c687e41222d8d73c2db8029ac118f
yujeong <www_17@naver.com>
2023-03-20 23:59:12 +0900

    Feat: AdminService getReportList, getQnaList 메서드 추가

--- src/main/java/com/spring/usMarket/service/admin/AdminService.java
+++ src/main/java/com/spring/usMarket/service/admin/AdminService.java
@@ -3,8 +3,12 @@ package com.spring.usMarket.service.admin;
 import java.util.List;
 import java.util.Map;
 
+import com.spring.usMarket.utils.AdminSearchCondition;
+
 public interface AdminService {
 	Map<String, Object> getAdmin(String admin_id, String admin_password) throws Exception;
-	List<Map<String, Object>> getMemberStatsPreview(String startDate, String endDate) throws Exception;
+	List<Map<String, Object>> getMemberStatsPreview(AdminSearchCondition adminSearchCondition) throws Exception;
 	List<Map<String, Object>> getDealStatsPreview(String startDate, String endDate) throws Exception;
+	List<Map<String, Object>> getReportList(String startPage, String endPage) throws Exception;
+	List<Map<String, Object>> getQnaList(String startPage, String endPage) throws Exception;
 }
