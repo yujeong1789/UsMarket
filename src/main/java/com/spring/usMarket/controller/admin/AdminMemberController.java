0563be6fb9f6cf7e731862fb99da4ce100bc2bcc
yujeong <www_17@naver.com>
2023-03-22 19:19:34 +0900

    Refactor: AdminMemberController memberList 메서드 수정

--- src/main/java/com/spring/usMarket/controller/admin/AdminMemberController.java
+++ src/main/java/com/spring/usMarket/controller/admin/AdminMemberController.java
@@ -1,20 +1,53 @@
 package com.spring.usMarket.controller.admin;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.usMarket.service.admin.AdminService;
+import com.spring.usMarket.utils.AdminPageHandler;
+import com.spring.usMarket.utils.AdminSearchCondition;
 
 @Controller
 @RequestMapping("/admin/member")
 public class AdminMemberController {
 	private static final Logger logger = LoggerFactory.getLogger(AdminMemberController.class);
 
+	@Autowired AdminService adminService;
+	
 	@GetMapping("/list")
 	public void memberList(Model model) {
-		logger.info("member/list");
+		
+		AdminSearchCondition sc = new AdminSearchCondition();
+		sc.setPageSize(10);
+		logger.info("adminSearchCondition = {}", sc.toString());
+		
+		List<Map<String, Object>> memberList = new ArrayList<>();
+		int totalCnt = 0;
+		
+		try {			
+			memberList = adminService.getMemberList(sc);
+			totalCnt = adminService.getMemberCnt(sc.getStartDate(), sc.getEndDate(), sc.getCondition());
+			AdminPageHandler pageHandler = new AdminPageHandler(totalCnt, sc);
+			 
+			model.addAttribute("memberList", memberList);
+			model.addAttribute("page", sc.getPage());
+			model.addAttribute("pageSize", sc.getPageSize());
+			model.addAttribute("ph", pageHandler);
+			 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@GetMapping("/info")
