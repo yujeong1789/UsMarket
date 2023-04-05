1dbab3f2af761654a33aeb6ca2163d6710d2a8b2
yujeong <www_17@naver.com>
2023-04-05 21:20:04 +0900

    Feat: QnaDao 추가
    insertQna, searchQnaInfo, searchQnaList, searchQnaCnt 메서드 추가

---
+++ src/main/java/com/spring/usMarket/dao/qna/QnaDao.java
@@ -0,0 +1,14 @@
+package com.spring.usMarket.dao.qna;
+
+import java.util.List;
+import java.util.Map;
+
+import com.spring.usMarket.domain.qna.QnaInsertDto;
+import com.spring.usMarket.utils.QnaSearchCondition;
+
+public interface QnaDao {
+	int insertQna(QnaInsertDto dto) throws Exception;
+	Map<String, Object> searchQnaInfo(String qna_no) throws Exception;
+	List<Map<String, Object>> searchQnaList(QnaSearchCondition sc) throws Exception;
+	int searchQnaCnt(String member_no) throws Exception;
+}
