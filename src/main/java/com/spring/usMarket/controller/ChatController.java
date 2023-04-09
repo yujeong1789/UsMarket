875a16f8f04b3bc704b13606770b38ba2bbc9b2a
yujeong <www_17@naver.com>
2023-04-09 20:56:10 +0900

    Style: ChatController import 정리

--- src/main/java/com/spring/usMarket/controller/ChatController.java
+++ src/main/java/com/spring/usMarket/controller/ChatController.java
@@ -1,12 +1,10 @@
 package com.spring.usMarket.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
