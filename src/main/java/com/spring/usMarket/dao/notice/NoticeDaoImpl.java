64871d2b1ade95e12e255de204bdb7fcc82c8bdd
yujeong <www_17@naver.com>
2023-04-09 20:56:31 +0900

    Feat: NoticeDao 메소드 추가
    searchNoticeList, searchNoticeCnt

---
+++ src/main/java/com/spring/usMarket/dao/notice/NoticeDaoImpl.java
@@ -0,0 +1,27 @@
+package com.spring.usMarket.dao.notice;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.usMarket.utils.NoticeSearchCondition;
+
+@Repository
+public class NoticeDaoImpl implements NoticeDao{
+	@Autowired
+	private SqlSession session;
+	private static String namespace = "com.mybatis.mapper.notice.";
+	
+	@Override
+	public List<Map<String, Object>> searchNoticeList(NoticeSearchCondition sc) throws Exception {
+		return session.selectList(namespace+"searchNoticeList", sc);
+	}
+
+	@Override
+	public int searchNoticeCnt(String notice_status) throws Exception {
+		return session.selectOne(namespace+"searchNoticeCnt", notice_status);
+	}
+}
