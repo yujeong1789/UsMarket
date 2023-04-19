1f32fc07b2a166f65ac75acd99c012a9e81d9248
yujeong <www_17@naver.com>
2023-04-19 21:52:04 +0900

    Feat: DealController 컨트롤러 메소드 추가
    get list, post list, get listform, get info, post info 추가

--- src/main/java/com/spring/usMarket/controller/DealController.java
+++ src/main/java/com/spring/usMarket/controller/DealController.java
@@ -1,14 +1,23 @@
 package com.spring.usMarket.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.usMarket.service.deal.DealService;
+import com.spring.usMarket.utils.SessionParameters;
 
 @Controller
 @RequestMapping("/deal")
@@ -23,9 +32,55 @@ public class DealController {
 		logger.info("deal_no = {}", deal_no);
 	}
 	
-	@PostMapping("/list")
-	public void list(String member_no) {
+	@GetMapping("/list")
+	public void list(Model model, HttpServletRequest request) {
+		
+		String member_no = SessionParameters.getUserNo(request);
+		String state = "";
+		String condition = "buy";
+		
 		logger.info("member_no = {}", member_no);
+		
+		List<Map<String, Object>> dealList = new ArrayList<>();
+		try {
+			dealList = dealService.getDealList("", "buy", member_no);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		model.addAttribute("dealList", dealList);
+		model.addAttribute("state", state);
+		model.addAttribute("condition", condition);
+	}
+	
+	@PostMapping("/list")
+	public String list(String state, String condition, RedirectAttributes ratt, HttpServletRequest request) {
+		
+		String member_no = SessionParameters.getUserNo(request);
+		logger.info("state = {}, condition = {}, member_no = {}", state, condition, member_no);
+		
+		List<Map<String, Object>> dealList = new ArrayList<>();
+		try {
+			dealList = dealService.getDealList(state, condition, member_no);
+			ratt.addFlashAttribute("dealList", dealList);
+			ratt.addFlashAttribute("state", state);
+			ratt.addFlashAttribute("condition", condition);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return "redirect:/deal/listform";
+	}
+	
+	@GetMapping("/listform")
+	public void listform() {
+		logger.info("listform");
+	}
+	
+	@GetMapping("/info")
+	public void info() {
+		logger.info("deal/info");
 	}
 	
 	@PostMapping("/info")
