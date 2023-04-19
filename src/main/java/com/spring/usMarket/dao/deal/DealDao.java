7c9220964688e0fc1ade8466078bd2f2478bac4b
yujeong <www_17@naver.com>
2023-04-19 21:51:00 +0900

    Feat: DealDao searchDealList 메소드 추가

--- src/main/java/com/spring/usMarket/dao/deal/DealDao.java
+++ src/main/java/com/spring/usMarket/dao/deal/DealDao.java
@@ -1,8 +1,12 @@
 package com.spring.usMarket.dao.deal;
 
+import java.util.List;
+import java.util.Map;
+
 import com.spring.usMarket.domain.deal.DealInsertDto;
 
 public interface DealDao {
 	int insertDeal(DealInsertDto dto) throws Exception;
 	int updateAddress(String member_zipcode, String member_address, String member_address_detail, Integer customer_no) throws Exception;
+	List<Map<String, Object>> searchDealList(String state, String condition, String member_no) throws Exception;
 }
