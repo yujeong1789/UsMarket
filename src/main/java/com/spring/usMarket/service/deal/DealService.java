135e562cac6a35ad4509ccd405f5319f789c6648
yujeong <www_17@naver.com>
2023-04-25 21:48:32 +0900

    Feat: DealService 서비스 메소드 추가
    getReviewInfo, addReview, getChatRoom

--- src/main/java/com/spring/usMarket/service/deal/DealService.java
+++ src/main/java/com/spring/usMarket/service/deal/DealService.java
@@ -12,4 +12,7 @@ public interface DealService {
 	int modifyDealState(String deal_state, String deal_no) throws Exception;
 	int modifyDealCancel(String deal_cancel, String deal_no) throws Exception;
 	int modifyDeliveryState(Map<String, Object> map) throws Exception;
+	Map<String, Object> getReviewInfo(String deal_no) throws Exception;
+	int addReview(String deal_no, String review_content, String review_score) throws Exception;
+	String getChatRoom(String chat_member_1, String chat_member_2) throws Exception;
 }
