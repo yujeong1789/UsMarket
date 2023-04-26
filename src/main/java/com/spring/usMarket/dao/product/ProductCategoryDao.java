42e19b0caa8dfd41d8d29868b5f403f975ef93a8
yujeong <www_17@naver.com>
2023-04-27 00:41:03 +0900

    Refactor: ProductCategoryDao 파라미터 타입 수정
    Integer -> String

--- src/main/java/com/spring/usMarket/dao/product/ProductCategoryDao.java
+++ src/main/java/com/spring/usMarket/dao/product/ProductCategoryDao.java
@@ -7,6 +7,6 @@ import com.spring.usMarket.domain.product.ProductCategoryDto;
 
 public interface ProductCategoryDao {
 	List<Map<String, Object>> searchProductCategory1() throws Exception;
-	List<ProductCategoryDto> searchProductCategory2(Integer product_category1_no) throws Exception;
-	List<Map<String, Object>> searchCategory2(Integer product_category1_no) throws Exception;
+	List<ProductCategoryDto> searchProductCategory2(String product_category1_no) throws Exception;
+	List<Map<String, Object>> searchCategory2(String product_category1_no) throws Exception;
 }
